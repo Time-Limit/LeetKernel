b6dd938d80d477653f65242f3bb2b383e7fa6d11
zhumingxiao <zhumingxiao@ai123.ink>
2025-08-05 09:46:23 +0000

    fix examples/004.fp16.mm.ldmatrix.cu

--- examples/004.fp16.mm.ldmatrix.cu
+++ examples/004.fp16.mm.ldmatrix.cu
@@ -783,6 +783,254 @@ __global__ void fp16_mma_m8n8k16_ldmatrix_trans__overlap_global_2_sm__quadra_buf
 #undef sm_2_B_mma_reg
 }
 
+template<typename T, int BLOCK_TILE_M, int BLOCK_TILE_N, int WARP_TILE_M, int WARP_TILE_N>
+__global__ void fp16_mma_m8n8k16_ldmatrix_trans__overlap_global_2_sm__quadra_buffer__opt(
+  const T* A, const T* B, const T* C, int M, int N, int K)
+{
+  constexpr int WARP_COUNT   = BLOCK_TILE_M / WARP_TILE_M * BLOCK_TILE_N / WARP_TILE_N;
+  constexpr int THREAD_COUNT = WARP_COUNT * 32;
+
+  constexpr int LOOP_TILE_K         = 16;
+  constexpr int LDG_SM_BUFFER_SIZE  = 4;
+  constexpr int LDG_REG_BUFFER_SIZE = 2;
+  constexpr int LDG_REG_BUFFER_INDEX_0 = 0;
+  constexpr int LDG_REG_BUFFER_INDEX_1 = 1;
+  // The 64 elements of type T in each 8x8 matrix are stored consecutively in a single layer.
+  __shared__ T A_sm[LDG_SM_BUFFER_SIZE][2][BLOCK_TILE_M][LOOP_TILE_K / 2];
+  __shared__ T B_sm[LDG_SM_BUFFER_SIZE][LOOP_TILE_K / 8][BLOCK_TILE_N / 8][8][8];
+
+  static_assert(BLOCK_TILE_M * LOOP_TILE_K % THREAD_COUNT == 0);
+  static_assert(BLOCK_TILE_M * LOOP_TILE_K / THREAD_COUNT % 8 == 0);
+  constexpr int A_LDG_COUNT_PER_THREAD = BLOCK_TILE_M * LOOP_TILE_K / THREAD_COUNT;
+  constexpr int A_LDG_LOOP_COUNT       = A_LDG_COUNT_PER_THREAD / 8;
+  // clang-format off
+  // This is the thread layout of the same warp that loads matrix A, where each thread reads M1xK8 elements of type T at a
+  // loop iteration.
+  // T0  T16
+  // T1  T17
+  // T2  T18
+  // ... ...
+  // T14 T30
+  // T15 T31
+  // clang-format on
+  T A_ldg_reg[LDG_REG_BUFFER_SIZE][A_LDG_LOOP_COUNT][8];
+
+  static_assert(BLOCK_TILE_N * LOOP_TILE_K % THREAD_COUNT == 0);
+  static_assert(BLOCK_TILE_N * LOOP_TILE_K / THREAD_COUNT % 8 == 0);
+  constexpr int B_LDG_COUNT_PER_THREAD = BLOCK_TILE_N * LOOP_TILE_K / THREAD_COUNT;
+  constexpr int B_LDG_LOOP_COUNT       = B_LDG_COUNT_PER_THREAD / 8;
+  // clang-format off
+  // This is the thread layout of the same warp that loads matrix B, where each thread reads K1xN8 elements of type T at a
+  // loop iteration.
+  // T0  T16
+  // T1  T17
+  // T2  T18
+  // ... ...
+  // T14 T30
+  // T15 T31
+  // clang-format on
+  T B_ldg_reg[LDG_REG_BUFFER_SIZE][B_LDG_LOOP_COUNT][8];
+
+  const int m_block_offset = blockIdx.y * BLOCK_TILE_M;
+  const int n_block_offset = blockIdx.x * BLOCK_TILE_N;
+
+  const int warp_id = threadIdx.x / 32;
+  const int lane_id = threadIdx.x % 32;
+
+  constexpr int M_MMA_WARP_COUNT       = BLOCK_TILE_M / WARP_TILE_M;
+  constexpr int M_GROUP_COUNT_PER_WARP = WARP_TILE_M / 8;
+  constexpr int N_GROUP_COUNT_PER_WARP = WARP_TILE_N / 16;
+
+  constexpr int MMA_REG_BUFFER_SIZE    = 2;
+  constexpr int MMA_REG_BUFFER_INDEX_0 = 0;
+  constexpr int MMA_REG_BUFFER_INDEX_1 = 1;
+  T             A_mma_reg[MMA_REG_BUFFER_SIZE][M_GROUP_COUNT_PER_WARP][4];
+  T             B_mma_reg[MMA_REG_BUFFER_SIZE][N_GROUP_COUNT_PER_WARP][8];
+  float         C_mma_reg[M_GROUP_COUNT_PER_WARP][N_GROUP_COUNT_PER_WARP][4] = {0};
+
+  const int m_warp_offset = warp_id % M_MMA_WARP_COUNT * WARP_TILE_M;
+  const int n_warp_offset = warp_id / M_MMA_WARP_COUNT * WARP_TILE_N;
+
+#define global_2_ldg_reg(k_loop_offset, ldg_reg_buffer_index)                                                          \
+  {                                                                                                                    \
+    for (int loop = 0; loop < A_LDG_LOOP_COUNT; ++loop) {                                                              \
+      const int m = (loop * WARP_COUNT + warp_id) * 16 + lane_id % 16;                                                 \
+      const int k = lane_id / 16 * 8;                                                                                  \
+      FETCH_FLOAT4(A_ldg_reg[ldg_reg_buffer_index][loop][0], A[OFFSET(m_block_offset + m, (k_loop_offset) + k, K)]);   \
+    }                                                                                                                  \
+    for (int loop = 0; loop < B_LDG_LOOP_COUNT; ++loop) {                                                              \
+      const int k = lane_id % 16;                                                                                      \
+      const int n = (loop * WARP_COUNT + warp_id) * 16 + lane_id / 16 * 8;                                             \
+      FETCH_FLOAT4(B_ldg_reg[ldg_reg_buffer_index][loop][0], B[OFFSET((k_loop_offset) + k, n_block_offset + n, K)]);   \
+    }                                                                                                                  \
+  }
+
+#define ldg_reg_2_sm(ldg_sm_buffer_index, ldg_reg_buffer_index)                                                        \
+  {                                                                                                                    \
+    for (int loop = 0; loop < A_LDG_LOOP_COUNT; ++loop) {                                                              \
+      const int m = (loop * WARP_COUNT + warp_id) * 16 + lane_id % 16;                                                 \
+      const int k = lane_id / 16 * 8;                                                                                  \
+      STORE_FLOAT4(A_sm[ldg_sm_buffer_index][k / 8][m][0], A_ldg_reg[ldg_reg_buffer_index][loop][0]);                  \
+    }                                                                                                                  \
+    for (int loop = 0; loop < B_LDG_LOOP_COUNT; ++loop) {                                                              \
+      const int k = lane_id % 16;                                                                                      \
+      const int n = (loop * WARP_COUNT + warp_id) * 16 + lane_id / 16 * 8;                                             \
+      STORE_FLOAT4(B_sm[ldg_sm_buffer_index][k / 8][n / 8][k % 8][0], B_ldg_reg[ldg_reg_buffer_index][loop][0]);       \
+    }                                                                                                                  \
+  }
+
+#define sm_2_A_mma_reg(ldg_sm_buffer_index, mma_reg_buffer_index, group)                                               \
+  /* for (int group = 0; group < M_GROUP_COUNT_PER_WARP; ++group) */ {                                                 \
+    uint32_t src =                                                                                                     \
+      __cvta_generic_to_shared(&A_sm[ldg_sm_buffer_index][lane_id / 8][m_warp_offset + (group) * 8 + lane_id % 8][0]); \
+    asm volatile("ldmatrix.sync.aligned.m8n8.x2.shared.b16 {%0, %1}, [%2];"                                            \
+                 : "=r"(*(uint32_t*)&A_mma_reg[mma_reg_buffer_index][group][0]),                                       \
+                   "=r"(*(uint32_t*)&A_mma_reg[mma_reg_buffer_index][group][2])                                        \
+                 : "r"(src));                                                                                          \
+  }
+
+#define sm_2_B_mma_reg(ldg_sm_buffer_index, mma_reg_buffer_index, group)                                               \
+  /*for (int group = 0; group < N_GROUP_COUNT_PER_WARP; ++group) */ {                                                  \
+    uint32_t src =                                                                                                     \
+      __cvta_generic_to_shared(&B_sm[ldg_sm_buffer_index][lane_id % 16 / 8]                                            \
+                                    [(n_warp_offset + (group) * 16 + lane_id / 16 * 8) / 8][lane_id % 8][0]);          \
+    asm volatile("ldmatrix.sync.aligned.m8n8.x4.trans.shared.b16 {%0, %1, %2, %3}, [%4];"                              \
+                 : "=r"(*(uint32_t*)&B_mma_reg[mma_reg_buffer_index][group][0]),                                       \
+                   "=r"(*(uint32_t*)&B_mma_reg[mma_reg_buffer_index][group][4]),                                       \
+                   "=r"(*(uint32_t*)&B_mma_reg[mma_reg_buffer_index][group][2]),                                       \
+                   "=r"(*(uint32_t*)&B_mma_reg[mma_reg_buffer_index][group][6])                                        \
+                 : "r"(src));                                                                                          \
+  }
+
+  global_2_ldg_reg(0, LDG_REG_BUFFER_INDEX_0);
+  global_2_ldg_reg(LOOP_TILE_K, LDG_REG_BUFFER_INDEX_1);
+  ldg_reg_2_sm(0, LDG_REG_BUFFER_INDEX_0);
+  ldg_reg_2_sm(1, LDG_REG_BUFFER_INDEX_1);
+  __syncthreads();
+
+  int LDG_SM_BUFFER_INDEX = 0;
+  int k_loop_offset = LOOP_TILE_K * 2;
+
+  for (int mg = 0; mg < M_GROUP_COUNT_PER_WARP; ++mg) {
+    sm_2_A_mma_reg(LDG_SM_BUFFER_INDEX, MMA_REG_BUFFER_INDEX_0, mg);
+  }
+  for (int ng = 0; ng < N_GROUP_COUNT_PER_WARP; ++ng) {
+    sm_2_B_mma_reg(LDG_SM_BUFFER_INDEX, MMA_REG_BUFFER_INDEX_0, ng);
+  }
+
+  while (k_loop_offset < K) {
+    global_2_ldg_reg(k_loop_offset, LDG_REG_BUFFER_INDEX_0);
+    global_2_ldg_reg(k_loop_offset + LOOP_TILE_K, LDG_REG_BUFFER_INDEX_1);
+
+    if constexpr (M_GROUP_COUNT_PER_WARP <= N_GROUP_COUNT_PER_WARP) {
+      for (int mg = 0; mg < M_GROUP_COUNT_PER_WARP; ++mg) {
+        for (int ng = 0; ng < M_GROUP_COUNT_PER_WARP; ++ng) {
+          mma_m16n8k16_row_col(C_mma_reg[mg][ng],
+                               B_mma_reg[MMA_REG_BUFFER_INDEX_0][ng],
+                               A_mma_reg[MMA_REG_BUFFER_INDEX_0][mg],
+                               C_mma_reg[mg][ng]);
+        }
+        sm_2_A_mma_reg(LDG_SM_BUFFER_INDEX + 1, MMA_REG_BUFFER_INDEX_1, mg);
+        sm_2_B_mma_reg(LDG_SM_BUFFER_INDEX + 1, MMA_REG_BUFFER_INDEX_1, mg);
+      }
+      for (int ng = M_GROUP_COUNT_PER_WARP; ng < N_GROUP_COUNT_PER_WARP; ++ng) {
+        for (int mg = 0; mg < M_GROUP_COUNT_PER_WARP; ++mg) {
+          mma_m16n8k16_row_col(C_mma_reg[mg][ng],
+                               B_mma_reg[MMA_REG_BUFFER_INDEX_0][ng],
+                               A_mma_reg[MMA_REG_BUFFER_INDEX_0][mg],
+                               C_mma_reg[mg][ng]);
+        }
+        sm_2_A_mma_reg(LDG_SM_BUFFER_INDEX + 1, MMA_REG_BUFFER_INDEX_1, ng);
+        sm_2_B_mma_reg(LDG_SM_BUFFER_INDEX + 1, MMA_REG_BUFFER_INDEX_1, ng);
+      }
+    }
+
+    LDG_SM_BUFFER_INDEX ^= 2;
+    k_loop_offset += LOOP_TILE_K * 2;
+    ldg_reg_2_sm(LDG_SM_BUFFER_INDEX, LDG_REG_BUFFER_INDEX_0);
+    ldg_reg_2_sm(LDG_SM_BUFFER_INDEX + 1, LDG_REG_BUFFER_INDEX_1);
+
+    __syncthreads();
+
+    if constexpr (M_GROUP_COUNT_PER_WARP <= N_GROUP_COUNT_PER_WARP) {
+      for (int mg = 0; mg < M_GROUP_COUNT_PER_WARP; ++mg) {
+        for (int ng = 0; ng < M_GROUP_COUNT_PER_WARP; ++ng) {
+          mma_m16n8k16_row_col(C_mma_reg[mg][ng],
+                               B_mma_reg[MMA_REG_BUFFER_INDEX_1][ng],
+                               A_mma_reg[MMA_REG_BUFFER_INDEX_1][mg],
+                               C_mma_reg[mg][ng]);
+        }
+        sm_2_A_mma_reg(LDG_SM_BUFFER_INDEX, MMA_REG_BUFFER_INDEX_0, mg);
+        sm_2_B_mma_reg(LDG_SM_BUFFER_INDEX, MMA_REG_BUFFER_INDEX_0, mg);
+      }
+      for (int ng = M_GROUP_COUNT_PER_WARP; ng < N_GROUP_COUNT_PER_WARP; ++ng) {
+        for (int mg = 0; mg < M_GROUP_COUNT_PER_WARP; ++mg) {
+          mma_m16n8k16_row_col(C_mma_reg[mg][ng],
+                               B_mma_reg[MMA_REG_BUFFER_INDEX_1][ng],
+                               A_mma_reg[MMA_REG_BUFFER_INDEX_1][mg],
+                               C_mma_reg[mg][ng]);
+        }
+        sm_2_A_mma_reg(LDG_SM_BUFFER_INDEX, MMA_REG_BUFFER_INDEX_0, ng);
+        sm_2_B_mma_reg(LDG_SM_BUFFER_INDEX, MMA_REG_BUFFER_INDEX_0, ng);
+      }
+    }
+  }
+  {
+    static_assert(M_GROUP_COUNT_PER_WARP == N_GROUP_COUNT_PER_WARP);
+    for (int mg = 0; mg < M_GROUP_COUNT_PER_WARP; ++mg) {
+      for (int ng = 0; ng < N_GROUP_COUNT_PER_WARP; ++ng) {
+        mma_m16n8k16_row_col(C_mma_reg[mg][ng],
+                             B_mma_reg[MMA_REG_BUFFER_INDEX_0][ng],
+                             A_mma_reg[MMA_REG_BUFFER_INDEX_0][mg],
+                             C_mma_reg[mg][ng]);
+      }
+      sm_2_A_mma_reg(LDG_SM_BUFFER_INDEX + 1, MMA_REG_BUFFER_INDEX_1, mg);
+      sm_2_B_mma_reg(LDG_SM_BUFFER_INDEX + 1, MMA_REG_BUFFER_INDEX_1, mg);
+    }
+
+    for (int mg = 0; mg < M_GROUP_COUNT_PER_WARP; ++mg) {
+      for (int ng = 0; ng < N_GROUP_COUNT_PER_WARP; ++ng) {
+        mma_m16n8k16_row_col(C_mma_reg[mg][ng],
+                             B_mma_reg[MMA_REG_BUFFER_INDEX_1][ng],
+                             A_mma_reg[MMA_REG_BUFFER_INDEX_1][mg],
+                             C_mma_reg[mg][ng]);
+      }
+    }
+  }
+
+  for (int mg = 0; mg < M_GROUP_COUNT_PER_WARP; ++mg) {
+    for (int ng = 0; ng < N_GROUP_COUNT_PER_WARP; ++ng) {
+      // int m = m_block_offset + m_warp_offset + mg * 8;
+      // int n = n_block_offset + n_warp_offset + ng * 16;
+      T casted[4] = {
+        C_mma_reg[mg][ng][0],
+        C_mma_reg[mg][ng][1],
+        C_mma_reg[mg][ng][2],
+        C_mma_reg[mg][ng][3],
+      };
+      uint32_t& first  = *(uint32_t*)(&casted[0]);
+      uint32_t& second = *(uint32_t*)(&casted[2]);
+      uint32_t  swap   = (first ^ second) * (!(lane_id & 0x4));
+      first ^= swap;
+      second ^= swap;
+      first = __shfl_xor_sync(0xffffffff, first, 0x4);
+      swap  = (first ^ second) * (!(lane_id & 0x4));
+      first ^= swap;
+      second ^= swap;
+      T                store[4]           = {casted[0], casted[2], casted[1], casted[3]};
+      static const int lane_2_n_offset[8] = {0, 8, 2, 10, 4, 12, 6, 14};
+      int              m                  = m_block_offset + m_warp_offset + mg * 8 + lane_id % 4 * 2;
+      int              n                  = n_block_offset + n_warp_offset + ng * 16 + lane_2_n_offset[lane_id / 4];
+      STORE_FLOAT(C[OFFSET(m, n, N)], store[0]);
+      STORE_FLOAT(C[OFFSET(m + 1, n, N)], store[2]);
+    }
+  }
+#undef global_2_ldg_reg
+#undef ldg_reg_2_sm
+#undef sm_2_A_mma_reg
+#undef sm_2_B_mma_reg
+}
+
 #define define_check_function(function)                                                                                                                  \
   template<typename T, int BLOCK_TILE_M, int BLOCK_TILE_N, int WARP_TILE_M, int WARP_TILE_N>                                                             \
   void launch_##function(const T* A, const T* B, T* C, int M, int N, int K)                                                                              \
@@ -841,6 +1089,7 @@ define_check_function(fp16_mma_m8n8k16_ldmatrix);
 define_check_function(fp16_mma_m8n8k16_ldmatrix_trans);
 define_check_function(fp16_mma_m8n8k16_ldmatrix_trans__overlap_global_2_sm);
 define_check_function(fp16_mma_m8n8k16_ldmatrix_trans__overlap_global_2_sm__quadra_buffer);
+define_check_function(fp16_mma_m8n8k16_ldmatrix_trans__overlap_global_2_sm__quadra_buffer__opt);
 /* clang-format on */
 
 template<typename T, typename = std::enable_if_t<std::is_same<T, half>::value || std::is_same<T, __nv_bfloat16>::value>>
@@ -876,6 +1125,7 @@ int test(const std::vector<float>& host_A,
   fp16_mma_m8n8k16_ldmatrix_trans___check_relative_error(fp16_A, fp16_B, fp16_C, M, N, K, host_C);
   fp16_mma_m8n8k16_ldmatrix_trans__overlap_global_2_sm___check_relative_error(fp16_A, fp16_B, fp16_C, M, N, K, host_C);
   fp16_mma_m8n8k16_ldmatrix_trans__overlap_global_2_sm__quadra_buffer___check_relative_error(fp16_A, fp16_B, fp16_C, M, N, K, host_C);
+  fp16_mma_m8n8k16_ldmatrix_trans__overlap_global_2_sm__quadra_buffer__opt___check_relative_error(fp16_A, fp16_B, fp16_C, M, N, K, host_C);
   /* clang-format on */
 
   CHECK_CUDA_RETURN(cudaFree(fp16_A));
